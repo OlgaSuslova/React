f2063f7ed07448450a99029c606eb9bd04b896a6
OlgaSuslova <alekssportmen@gmail.com>
2024-04-25 11:11:37 +0300

    added homework 1

---
+++ hw_1/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Message from './components/Message';
+
+function App() {
+   
+  return (
+    <div className="App">      
+        <Message userMessage="Привет!" />
+        <Message userMessage="Как дела?" />      
+    </div>
+  );
+}
+
+export default App;
+
+
+
---
+++ hw_1/src/components/Message.js
@@ -0,0 +1,9 @@
+function Message ({userMessage}) {
+    return ( 
+        <div className="user-message">
+            <p>{userMessage}</p>
+        </div>
+     );
+}
+
+export default Message ;
